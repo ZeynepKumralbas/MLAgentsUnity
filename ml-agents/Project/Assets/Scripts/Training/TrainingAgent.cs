62128065629725ea0cc99ba1b756cc5a7a8e6380
zeynepkumralbas <zeynepkumralbas@marun.edu.tr>
2025-08-24 23:58:20 +0300

    Add ml-agents as folder instead of submodule

---
+++ ml-agents/Project/Assets/Scripts/Training/TrainingAgent.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class TrainingAgent : Agent
+{
+    private Rigidbody rbody;
+    public Transform target;
+    public float multiplier = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();    
+    }
+
+    public override void OnEpisodeBegin(){
+        if (transform.localPosition.y < 0){
+            rbody.angularVelocity = Vector3.zero;
+            rbody.linearVelocity = Vector3.zero;
+            transform.localPosition = new Vector3(0, 0.5f, 0f);
+        }
+
+        target.localPosition = new Vector3(Random.value * 8.5f - 4, 0.5f, Random.value * 8.5f - 4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor){
+        sensor.AddObservation(target.localPosition);
+        sensor.AddObservation(transform.localPosition);
+
+        sensor.AddObservation(rbody.linearVelocity.x);
+        sensor.AddObservation(rbody.linearVelocity.z);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions){
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actions.ContinuousActions[0];
+        controlSignal.z = actions.ContinuousActions[1];
+        rbody.AddForce(controlSignal * multiplier);
+
+        float distanceToTarget = Vector3.Distance(transform.localPosition, target.localPosition);
+        if(distanceToTarget < 1.5f){
+            SetReward(1.0f);
+            EndEpisode();
+        }
+        if(transform.localPosition.y < 0f){
+            SetReward(-1f);
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut){
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        continuousActionsOut[1] = Input.GetAxis("Vertical");        
+    }
+}
